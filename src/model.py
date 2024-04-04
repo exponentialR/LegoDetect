bf44eecf9bf087be83645f66f61b1f67d48bbe11
Samuel Adebayo <soluadebayo@gmail.com>
2024-04-04 20:07:45 +0100

    yolov8n training

--- src/model.py
+++ src/model.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='config.yaml', epochs=100, imgsz=640, devices='mps')
+results = model.train(data='config.yaml', epochs=100, imgsz=640)
